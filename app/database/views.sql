6cfe9954793e480a0ba68c1a05e20886bd287840
gmiak <kayss.g@gmail.com>
2021-02-04 08:42:44 +0100

    Added in views.sql a new version of View PathToGraduation (not complete)

--- app/database/views.sql
+++ app/database/views.sql
@@ -1,13 +1,11 @@
-DROP VIEW BasicInformation;
-CREATE VIEW BasicInformation AS
+CREATE OR REPLACE VIEW BasicInformation AS
 SELECT idnr, name, login, Students.program, COALESCE(branch,'No yet') AS branch FROM Students FULL OUTER JOIN StudentBranches ON idnr=student;
 SELECT* FROM BasicInformation;
---DROP VIEW BasicInformation;
 
-CREATE VIEW FinishedCourses AS
+
+CREATE OR REPLACE VIEW FinishedCourses AS
 SELECT student, course, grade, Courses.credits FROM Taken LEFT OUTER JOIN Courses ON code= course;
 SELECT* FROM FinishedCourses;
-DROP VIEW FinishedCourses;
 
 /*CREATE VIEW StudentCourses AS
 SELECT idnr AS student, COALESCE(course,'No course') AS course , COALESCE(grade,'No grade') AS grade ,  COALESCE(credits,0) AS
@@ -15,20 +13,18 @@ credits  FROM Students LEFT OUTER JOIN Taken ON idnr= student LEFT OUTER JOIN Co
 SELECT* FROM StudentCourses;
 DROP VIEW StudentCourses;*/
 
-DROP VIEW PassedCourses;
-CREATE VIEW PassedCourses AS
+
+CREATE OR REPLACE VIEW PassedCourses AS
 SELECT student, course, Courses.credits FROM Taken LEFT OUTER JOIN Courses ON code= course
 WHERE grade != 'U';
 SELECT* FROM PassedCourses;
---DROP VIEW PassedCourses;
 
-CREATE VIEW Registrations AS
+
+CREATE OR REPLACE VIEW Registrations AS
 SELECT student, course, 'registered' AS status FROM Registered
 UNION ALL
 SELECT student,course, 'Waiting' AS status FROM WaitingList;
-
 SELECT* FROM Registrations;
-DROP VIEW Registrations;
 
 /*CREATE VIEW UnreadMandatory AS
 SELECT student, Taken.course FROM Taken FULL OUTER JOIN MandatoryProgram ON Taken.course= MandatoryProgram.course
@@ -48,14 +44,25 @@ MandatoryBranch.branch= BasicInformation.branch;
 SELECT* FROM UnreadMandatory;
 DROP VIEW UnreadMandatory;*/
 
-CREATE VIEW UnreadMandatory AS
+
+CREATE OR REPLACE VIEW UnreadMandatory AS
 (SELECT idnr AS student, course  FROM BasicInformation RIGHT OUTER JOIN MandatoryProgram ON
 MandatoryProgram.program = BasicInformation.program
 UNION
 SELECT idnr AS student, course  FROM BasicInformation RIGHT OUTER JOIN MandatoryBranch ON
-MandatoryBranch.branch= BasicInformation.branch)
+MandatoryBranch.branch = BasicInformation.branch)
 EXCEPT
 SELECT student, course FROM PassedCourses;
 SELECT* FROM UnreadMandatory;
-DROP VIEW UnreadMandatory;
-/** Kolla varför 6666666666 inte har någon mandatoryCourse**/
+SELECT student, COUNT(course) FROM UnreadMandatory GROUP BY student;
+
+
+CREATE VIEW PathToGraduation AS
+SELECT idnr AS student, COALESCE(SUM(credits), 0) AS totalCredits, COALESCE(COUNT(DISTINCT(UnreadMandatory.course)), 0) AS mandatoryLeft
+FROM BasicInformation
+LEFT OUTER JOIN PassedCourses ON BasicInformation.idnr=PassedCourses.student
+LEFT OUTER JOIN UnreadMandatory ON BasicInformation.idnr=UnreadMandatory.student
+GROUP BY BasicInformation.idnr;
+
+SELECT* FROM PathToGraduation;
+DROP VIEW PathToGraduation;
