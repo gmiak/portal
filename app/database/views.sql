68a625e12f337c232b7dbec886b7e849c239d52e
Alasiri-se <68913952+Alasiri-se@users.noreply.github.com>
2021-02-11 18:28:59 +0100

    ER diagram, Schema

--- app/database/views.sql
+++ app/database/views.sql
@@ -1,6 +1,6 @@
 ------------------- view BasicInformation ----------------------------------
 CREATE OR REPLACE VIEW BasicInformation AS
-SELECT idnr, name, login, Students.program, COALESCE(branch,'No yet') AS branch FROM Students FULL OUTER JOIN StudentBranches ON idnr=student;
+SELECT idnr, name, login, Students.program, branch FROM Students FULL OUTER JOIN StudentBranches ON idnr=student;
 --SELECT* FROM BasicInformation;
 
 ------------------- view FinishedCourses ----------------------------------
@@ -18,7 +18,7 @@ WHERE grade != 'U';
 CREATE OR REPLACE VIEW Registrations AS
 SELECT student, course, 'registered' AS status FROM Registered
 UNION ALL
-SELECT student,course, 'Waiting' AS status FROM WaitingList;
+SELECT student,course, 'waiting' AS status FROM WaitingList;
 --SELECT* FROM Registrations;
 
 ------------------- view UnreadMandatory ----------------------------------
